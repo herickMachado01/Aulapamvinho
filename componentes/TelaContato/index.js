b936e22ba31bc6359884d1db23cc1f8bf9223e56
herickMachado01 <160595828+herickMachado01@users.noreply.github.com>
2024-09-03 08:12:55 -0300

    mudança no codigo

--- componentes/TelaContato/index.js
+++ componentes/TelaContato/index.js
@@ -1,40 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons'; // ou outra coleção de ícones
+import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-
-import styles from './estilo'
+import styles from './estilo';
 
 export default function TelaContato() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.header}>Entre em contato conosco para comprar nossos produtos</Text>
-  
-        <View style={styles.card}>
-          <Icon name="phone" size={40} color="#800000" />
-          <Text style={styles.label}>Telefone:</Text>
-          <Text style={styles.info}>+55 21 030000000</Text>
-        </View>
-  
-        <View style={styles.card}>
-          <Icon name="place" size={40} color="#800000" />
-          <Text style={styles.label}>Endereço:</Text>
-          <Text style={styles.info}>Av. 123, 222 - Rio de Janeiro, RJ</Text>
-        </View>
-  
-        <View style={styles.card}>
-          <Icon name="email" size={40} color="#800000" />
-          <Text style={styles.label}>Email:</Text>
-          <Text style={styles.info}>preferida@adega.com.br</Text>
-        </View>
-  
-        <View style={styles.card}>
-          <Icon name="instagram" size={40} color="#800000" />
-          <Text style={styles.label}>Instagram:</Text>
-          <Text style={styles.info}>@adegapreferida</Text>
-        </View>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Entre em contato conosco para comprar nossos produtos</Text>
+
+      <View style={styles.card}>
+        <Icon name="phone" size={40} color="#800000" />
+        <Text style={styles.label}>Telefone:</Text>
+        <Text style={styles.info}>+55 21 030000000</Text>
       </View>
-    );
-  }
-  
+
+      <View style={styles.card}>
+        <Icon name="map-marker" size={40} color="#800000" />
+        <Text style={styles.label}>Endereço:</Text>
+        <Text style={styles.info}>Av. 123, 222 - Rio de Janeiro, RJ</Text>
+      </View>
+
+      <View style={styles.card}>
+        <Icon name="envelope" size={40} color="#800000" />
+        <Text style={styles.label}>Email:</Text>
+        <Text style={styles.info}>preferida@adega.com.br</Text>
+      </View>
+
+      <View style={styles.card}>
+        <Icon name="instagram" size={40} color="#800000" />
+        <Text style={styles.label}>Instagram:</Text>
+        <Text style={styles.info}>@adegapreferida</Text>
+      </View>
+    </View>
+  );
+}
+
 
